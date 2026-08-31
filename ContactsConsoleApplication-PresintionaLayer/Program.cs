4d9f46aedfd44c5c8a885a5ca94bad94f8aaaf3a
banaf <banafea2005a@gmail.com>
2026-08-31 10:50:43 +0300

    Test Find Method in ContactsConsoleLayer

--- ContactsConsoleApplication-PresintionaLayer/Program.cs
+++ ContactsConsoleApplication-PresintionaLayer/Program.cs
@@ -6,11 +6,37 @@ using System.Threading.Tasks;
 using ContactsBussinesLayer;
 namespace ContactsConsoleApplication_PresintionaLayer
 {
+    
     internal class Program
     {
+        static void TestFind(int ID)
+        {
+
+            clsContacts contact = clsContacts.Find(ID);
+
+
+            if (contact != null)
+            {
+                Console.WriteLine($"ContactID : {contact.ContactID}");
+                Console.WriteLine($"Name  : {contact.FirstName} {contact.LastName}");
+                Console.WriteLine($"Email : {contact.Email}");
+                Console.WriteLine($"Phone : {contact.Phone}");
+                Console.WriteLine($"Address : {contact.Address}");
+                Console.WriteLine("Date Of Birth : {0:yyyy-MM-dd}", contact.DateOfBirth);
+                Console.WriteLine($"CountryID : {contact.CountryID}");
+                Console.WriteLine($"ImagePath : {contact.ImagePath}");
+
+            }
+
+            else
+            {
+                Console.WriteLine($"Contact with ID {ID} not found.");
+            }
+
+        }
         static void Main(string[] args)
         {
-            
+            TestFind(1);
         }
     }
 }
