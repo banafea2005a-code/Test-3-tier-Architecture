4d0f367d47ac8b780c714da0fccb85b508d472c7
banaf <banafea2005a@gmail.com>
2026-08-31 09:30:09 +0300

    intial project

---
+++ ContactsBussinesLayer/clsContacts.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContactsDataAccessLayer;
+
+namespace ContactsBussinesLayer
+{
+    public class clsContacts
+    {
+        enum enMode { AddNew =0,Update=1 };
+        private enMode Mode = enMode.AddNew;
+        public int ContactID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int CountryID { get; set; }
+        public string ImagePath { get; set; }
+
+
+        
+
+    }
+}
---
+++ ContactsDataAccessLayer/ClsDataAccessSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsDataAccessLayer
+{
+    static class ClsDataAccessSettings
+    {
+
+        public static string ConnectionString = "server=.;database=ContactsDB;user id=sa;password=123456";
+
+    }
+}
