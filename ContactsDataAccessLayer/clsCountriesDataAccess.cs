ce96f56cfabae9bf3753a72744b0ad6a74443432
banaf <banafea2005a@gmail.com>
2026-09-02 12:48:03 +0300

    Add GetCountryInfo By ID method

--- ContactsDataAccessLayer/clsCountriesDataAccess.cs
+++ ContactsDataAccessLayer/clsCountriesDataAccess.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ContactsDataAccessLayer
 {
-    internal class clsCountriesDataAccess
+    static class clsCountriesDataAccess
     {
+        static bool GetCountryInfoByID(int ID, ref string CountryName)
+        {
+
+            SqlConnection connection = new SqlConnection(ClsDataAccessSettings.ConnectionString);
+            string query = "select * from Countries where CountryName = @CountryName";
+
+
+
+        }
     }
 }
