eed74669c5aac96b5e4d9a690cb9005266493967
banaf <banafea2005a@gmail.com>
2026-09-02 12:54:00 +0300

    Add Is Exist Method

--- ContactsDataAccessLayer/clsCountriesDataAccess.cs
+++ ContactsDataAccessLayer/clsCountriesDataAccess.cs
@@ -11,12 +11,80 @@ namespace ContactsDataAccessLayer
     {
         static bool GetCountryInfoByID(int ID, ref string CountryName)
         {
+            bool IsFound = false;
+
+
 
             SqlConnection connection = new SqlConnection(ClsDataAccessSettings.ConnectionString);
             string query = "select * from Countries where CountryName = @CountryName";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@CountryName", CountryName);
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+
+                    // Country Is Found
+                    IsFound = true;
+
+                    CountryName = (string)reader["CountryName"];
+                }
+                reader.Close();
+
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+
+                connection.Close();
+            }
+
+            return IsFound;
+        }
+    
+    static bool IsExist(int CountryID)
+        {
+
+            bool IsExist = false;
+
+            SqlConnection connection = new SqlConnection(ClsDataAccessSettings.ConnectionString);
+            string query = "select Found=1 from Countries where CountryID = @CountryID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@CountryID", CountryID);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    IsExist = reader.HasRows;
+                }
 
 
+            }
+            catch (Exception ex)
+            {
 
+
+            }
+
+            finally
+            {
+                connection.Close();
+
+            }
+            
+            return IsExist;
         }
     }
+
 }
